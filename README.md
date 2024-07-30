518f3e1d53019b649acd1f7a709c50708a187851
dantemayson <meh.far.hdi@gmail.com>
2024-07-30 13:26:37 +0330

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# StyleCop.Demo" 
---
+++ StyleCop.Demo/WeatherForecast.cs
@@ -0,0 +1,18 @@
+namespace StyleCop.Demo
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(this.TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+
+    public interface IWeatherForecast
+    {
+
+    }
+}
